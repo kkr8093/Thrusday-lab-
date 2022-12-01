489e844809e157b7c65416196d4990ed2f155e6b
kkr8093 <119559941+kkr8093@users.noreply.github.com>
2022-12-01 14:12:55 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,8 @@
+public class Circle
+{
+public static void main(String args[])
+{
+int r=80, p=10;
+System.out.println("The area of Circle is:"+(r*p));
+}
+}
---
+++ Perimiter_rect.java
@@ -0,0 +1,9 @@
+public class Perimiter_rect
+{
+public static void main(String args[])
+{
+int length=5, width=6;
+int perimiter = 2*(length+width);
+System.out.println("The perimiter of Rectangle is:"+ (perimiter));
+}
+}
